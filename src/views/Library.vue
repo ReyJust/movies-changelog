605a6cc379e32c53c8772b37857391c4bf98eef0
Justin <jusmayer@outlook.com>
2021-09-30 15:14:05 +0400

    Remove deleted movies

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -9,13 +9,10 @@
 import Movies from "../components/Movies.vue";
 import NewMovies from "../components/NewMovies.vue";
 
-// import DeleteMovies from "../components/DeletedMovies.vue";
-
 export default {
   components: {
     Movies,
     NewMovies,
-    // DeleteMovies,
   },
   data() {
     return {};
